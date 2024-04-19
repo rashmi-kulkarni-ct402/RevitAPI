75993966cd811de42199d9b7ef4c055d028e1b2b
rashmi-kulkarni-ct402 <rashmi.kulkarni@cctech.co.in>
2024-04-19 15:44:51 +0530

    initial commit

---
+++ RevitAPI/RevitAPIClass.cs
@@ -0,0 +1,24 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPI
+{
+    // transactions for this command managed manually
+    [Autodesk.Revit.Attributes.Transaction(Autodesk.Revit.Attributes.TransactionMode.Manual)]
+    public class RevitAPIClass : IExternalCommand
+    {    
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData revit,
+          ref string message, ElementSet elements)
+        {
+            // display dialog box
+            TaskDialog.Show("Hi Rashmi!", "This is your Revit API...");
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+    }
+}
