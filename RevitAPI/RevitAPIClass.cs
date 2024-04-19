e796e7ae975ccc7b8e13261346db7e2365e36218
rashmi-kulkarni-ct402 <rashmi.kulkarni@cctech.co.in>
2024-04-19 15:46:53 +0530

    updated "using" namespaces

--- RevitAPI/RevitAPIClass.cs
+++ RevitAPI/RevitAPIClass.cs
@@ -1,11 +1,5 @@
-﻿using Autodesk.Revit.Attributes;
-using Autodesk.Revit.DB;
+﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace RevitAPI
 {
